a5b8d9480d39157f05c5b845784319da5327cb1c
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2023-10-23 13:58:50 +0200

    add basic styling to card

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ todoList.push(task3);
 // Skapa HTML
 const container = document.querySelector(".container");
 const card = document.createElement("article");
+card.classList.add("card");
 const text = document.createElement("h3");
 let checkbox = document.createElement("input");
 checkbox.setAttribute("type", "checkbox");
@@ -32,6 +33,4 @@ card.appendChild(text);
 card.appendChild(checkbox);
 container.appendChild(card);
 
-// const text = document.createElement("h3");
-// container.appendChild(text);
 text.innerHTML = todoList[1].description;
