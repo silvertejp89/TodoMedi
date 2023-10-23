47f4a0df4f9bb7c6b088c30e7f18dee1d5b1aba3
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2023-10-23 10:34:02 +0200

    create class Task

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+
+class Task {
+  description;
+  done;
+
+  constructor(description, done) {
+    this.description = description;
+    this.done = done;
+  }
+}
+
+const task1 = new Task("Tvätta bilen", false);
+
+console.log(task1);
