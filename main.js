98387bd9c3f58bec9b965f9f20ea1cac444b00d3
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2023-10-23 13:49:04 +0200

    create html for one task

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import "./style.css";
 
+let todoList = [];
+
 class Task {
   description;
   done;
@@ -10,6 +12,26 @@ class Task {
   }
 }
 
+// Skapar några "existerande tasks"
 const task1 = new Task("Tvätta bilen", false);
+const task2 = new Task("Klippa gräset", false);
+const task3 = new Task("Städa altanen", false);
 
-console.log(task1);
+// Lägger tasks i todolist
+todoList.push(task1);
+todoList.push(task2);
+todoList.push(task3);
+
+// Skapa HTML
+const container = document.querySelector(".container");
+const card = document.createElement("article");
+const text = document.createElement("h3");
+let checkbox = document.createElement("input");
+checkbox.setAttribute("type", "checkbox");
+card.appendChild(text);
+card.appendChild(checkbox);
+container.appendChild(card);
+
+// const text = document.createElement("h3");
+// container.appendChild(text);
+text.innerHTML = todoList[1].description;
