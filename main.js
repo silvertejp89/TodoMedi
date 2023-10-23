364313b46566a0904403aa3d2558c1a993c3192a
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2023-10-23 14:48:34 +0200

    loop for creating html

--- main.js
+++ main.js
@@ -23,14 +23,33 @@ todoList.push(task2);
 todoList.push(task3);
 
 // Skapa HTML
-const container = document.querySelector(".container");
-const card = document.createElement("article");
-card.classList.add("card");
-const text = document.createElement("h3");
-let checkbox = document.createElement("input");
-checkbox.setAttribute("type", "checkbox");
-card.appendChild(text);
-card.appendChild(checkbox);
-container.appendChild(card);
+// const container = document.querySelector(".container");
+// const card = document.createElement("article");
+// card.classList.add("card");
+// const text = document.createElement("h3");
+// let checkbox = document.createElement("input");
+// checkbox.setAttribute("type", "checkbox");
+// card.appendChild(text);
+// card.appendChild(checkbox);
+// container.appendChild(card);
 
-text.innerHTML = todoList[1].description;
+// text.innerHTML = todoList[1].description;
+
+function createHTML(todoList) {
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+  for (let i = 0; i < todoList.length; i++) {
+    const card = document.createElement("article");
+    card.classList.add("card");
+    const text = document.createElement("h3");
+    let checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    card.appendChild(text);
+    card.appendChild(checkbox);
+    container.appendChild(card);
+
+    text.innerHTML = todoList[i].description;
+  }
+}
+
+createHTML(todoList);
